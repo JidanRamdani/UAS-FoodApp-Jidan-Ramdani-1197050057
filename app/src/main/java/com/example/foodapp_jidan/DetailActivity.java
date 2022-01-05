d0e78bd4f71d1392db2b74ab807ba5fd686978ec
Jidan Ramdani <85661500+JidanRamdani@users.noreply.github.com>
2022-01-05 09:49:39 +0700

    firstcommit

---
+++ app/src/main/java/com/example/foodapp_jidan/DetailActivity.java
@@ -0,0 +1,33 @@
+package com.example.foodapp_jidan;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        int id = getIntent().getIntExtra("id", -1);
+
+        ArrayList<com.example.foodapp_jidan.Food> list = com.example.foodapp_jidan.ListFood.getData(getApplicationContext());
+        com.example.foodapp_jidan.Food food = list.get(id);
+
+        if (food != null) {
+            TextView judul = findViewById(R.id.detail_judul);
+            TextView deskripsi = findViewById(R.id.detail_deskripsi);
+            ImageView gambar = findViewById(R.id.detail_gambar);
+
+            gambar.setImageDrawable(food.image);
+            judul.setText(food.title);
+            deskripsi.setText(food.description);
+        }
+    }
+}
